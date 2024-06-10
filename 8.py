6bdf8ddc666b2638cc180c40a1d0c4ef3aa1e044
Parnian Nazari <naazari2002@gmail.com>
2024-06-11 00:23:02 +0330

    .

--- 8.py
+++ 8.py
@@ -3,7 +3,7 @@ data = {"5":[22, 24, 20, 21],
         "14":[26, 25, 19, 23],
         "23":[25, 26, 19, 25]}
 y = pd.DataFrame(data, index=["Te", "Es", "Ta", "Sa"])
-w = pd.ExcelWriter("FirsT Exel.xlsx", engine="xlsxwriter")
+w = pd.ExcelWriter("FirsT Excel.xlsx", engine="xlsxwriter")
 y.to_excel(w, sheet_name="temp")
 w.save()
 
